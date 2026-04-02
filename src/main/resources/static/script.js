76818093dc946375b1620111a4254619d07dd696
joazis-quebra-codegos <joao.ds.zantti@gmail.com>
2026-04-01 22:21:35 -0300

    script.js. index.html

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,28 @@
-function salvar(){
-    const livro ={
-        titulo: document.getElementById("titulo").value;
+const url = "http://localhost:8080/livros"
 
-    }
+function salvar(){
+    const livro = {
+        titulo: document.getElementById("titulo").value,
+        autor: document.getElementById("autor").value,
+        preco: parseFloat(document.getElementById("preco").value),
+        estoque: parseInt(document.getElementById("estoque").value),
+        isbn: document.getElementById("isbn").value
+    };
+
+    fetch(url, {method: "POST", headers:{"Content-Type":"application/json"}, body: JSON.stringify(livro)}).then(() => listar())
+}
+
+function listar(){
+    fetch(url)
+        .then(res => res.json())
+        .then(dados => {
+            const lista = document.getElementById("lista");
+            lista.innerHTML = "";
+
+            dados.forEach(livro => {
+                const li = document.createElement("li");
+                li.innerText = `${livro.titulo} - ${livro.preco}`;
+                lista.appendChild(li);
+            });
+        });
 }
